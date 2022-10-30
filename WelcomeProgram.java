12d685787f1b907af46f079b33d852366ee6d357
Sourav <souravkhare34@gmail.com>
2022-10-31 00:51:50 +0530

    Welcome Program for line comparision on master

---
+++ WelcomeProgram.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.day8_linecomparision;
+
+public class WelcomeProgram {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+}
+
